c46ac26500115c3f53ada7c57aaa92bbe737b809
Vinicius Goncalves <oldz.registros@gmail.com>
2022-06-03 20:58:06 -0300

    Updating register.css and register.html

--- js/register.js
+++ js/register.js
@@ -1,3 +1,13 @@
 import { auth } from './authAndRequests.js'
-import { createUserWithPasswordAndEmail } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js"
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js"
+import { userNavbar } from './userExperience.js'
 
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js"
+
+onAuthStateChanged(auth, (user) => {
+    if(user) {
+
+    }
+})
+
+const registerContainer = document.querySelector('[data-js="register-container"]')
